8e426efe0435158d6bac5115299abf795176a1cf
Eric Pierce <wapcaplet88@gmail.com>
2011-07-06 14:21:11 -0600

    Added robin as a Rails 3.1.0.rc4 app

---
+++ robin/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Robin::Application.config.secret_token = '54cf0adc912e23d45d1c163e45236cb2c0328f896f7287ea00a3e51be7ee9080a8064d947f93928ddc02bb3a0d777519a2339b0fbe0a90c33256b21298c545cf'
